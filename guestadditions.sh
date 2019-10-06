47d63062be0641012bed3b7ee853c2f081f8ed28
Pavel Stetina <github@shark.kom.cz>
2019-10-06 22:23:16 +0200

    Fix controller to IDE Better debugging info

--- guestadditions.sh
+++ guestadditions.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 function getVersion {
-	local VERSION=$(vagrant ssh $1 -c "sudo modinfo vboxguest | awk '/^version/ { print \$2 }' | tr -dc '[[:print:]]'" < /dev/null)
+	local VERSION=$(vagrant ssh $1 -c "sudo modinfo vboxguest | awk '/^ver/ { print \$2 }' | tr -dc '[[:print:]]'" < /dev/null)
 	echo $VERSION
 }
 
 function isCurrentVersion {
 	local VERSION=$(getVersion $1)
-	echo $VERSION '>=' $VERSION_TO_INSTALL
+	echo -e "\e[7m$VERSION" '>=' $VERSION_TO_INSTALL
 	[ -n "$VERSION" ] && dpkg --compare-versions $VERSION '>=' $2
 }
 
@@ -36,28 +36,29 @@ function run {
 		VBOXVM=$VM
 		echo "$VM: $VM_STATUS, $PROVIDER";
 		if [[ $VM_STATUS == 'not created' ]]; then
-			echo "Bring up '$VM' because it's not created"
+			echo -e "\e[7mBring up '$VM' because it's not created"
 			vagrantUpLimited
 		fi
 		if isCurrentVersion $VM $VERSION_TO_INSTALL; then
-			echo "Skip '$VM' - current installed version is equal or greater"
+			echo -e "\e[7mSkip '$VM' - current installed version is equal or greater"
 		else
+			echo -e "\e[7m\nInstall new version to '$VM'\n"
 			if [[ $VM_STATUS != 'poweroff' ]] ; then
 				echo "Halting '$VM'"
 				vagrant halt $VM
 			fi
-			echo "Attach ISO to '$VM' and bring machine up"
-			vboxmanage storageattach $VBOXVM --storagectl 'SATA Controller' --port 1 --device 0 --type dvddrive --medium /usr/share/virtualbox/VBoxGuestAdditions.iso
+			echo -e "\e[7mAttach ISO to '$VM' and bring machine up"
+			vboxmanage storageattach $VBOXVM --storagectl 'IDE Controller' --port 1 --device 0 --type dvddrive --medium /usr/share/virtualbox/VBoxGuestAdditions.iso
 			vagrantUpLimited
 			if isCurrentVersion $VM $VERSION_TO_INSTALL; then
-				echo "Skip '$VM' because current installed version is equal or greater"
+				echo -e "\e[7mSkip '$VM' because current installed version is equal or greater"
 			else
-				echo "Build guest additions on '$VM'"
-				vagrant ssh $VM -c 'sudo apt-get install -y build-essential dkms linux-headers-$(uname -r) && sudo mount /dev/cdrom /mnt && cd /mnt && sudo ./VBoxLinuxAdditions.run' < /dev/null
+				echo -e "\e[7mBuild guest additions on '$VM'"
+				vagrant ssh $VM -c 'sudo apt-get install -y build-essential dkms linux-headers-$(uname -r) && sudo mount /dev/cdrom /mnt && cd /mnt && sudo ./VBoxLinuxAdditions.run -- force' < /dev/null
 			fi
-			echo "Halting machine '$VM' and detach ISO"
+			echo -e "\e[7mHalting machine '$VM' and detach ISO"
 			vagrant halt $VM
-			vboxmanage storageattach $VBOXVM --storagectl 'SATA Controller' --port 1 --device 0 --type dvddrive --medium none
+			vboxmanage storageattach $VBOXVM --storagectl 'IDE Controller' --port 1 --device 0 --type dvddrive --medium none
 		fi
 		if [[ $VM_STATUS == 'running' ]]; then
 			vagrant up $VM
@@ -72,7 +73,7 @@ export -f run isCurrentVersion getVersion vagrantUpLimited
 export VERSION_TO_INSTALL=$(dpkg -l | grep virtualbox-guest-additions | grep -oP '\d+\.\d+\.\d+')
 VM_STATUSES=$(vagrant status | grep -P '^\w+(?=\s{2,})')
 VMS_TO_INSTALL=$@
-echo 'Running guest additions install'
+echo -e '\e[7mRunning guest additions install'
 printf "%s\n" "${VM_STATUSES[@]}"
 echo "Install on ${VMS_TO_INSTALL:-all machines}"
 #printf "%s\n" "${VM_STATUSES[@]}" | parallel --jobs 0 run {} "'${VMS_TO_INSTALL[@]}'"
